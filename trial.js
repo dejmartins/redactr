c0084436b970970e2f426442cb6de16026620d7b
Jonathan Martins <dejimartins99@gmail.com>
2022-07-27 20:38:34 +0100

    trial

--- trial.js
+++ trial.js
@@ -3,10 +3,11 @@ console.log("Hello world");
 let sentence = "Hel World Bro Beast";
 let toScramble = "Hel Beast";
 toScramble = toScramble.split(" ");
+let replacement = "*";
 
 for (word of toScramble) {
   if (sentence.includes(word)) {
-    sentence = sentence.replace(word, "*".repeat(word.length));
+    sentence = sentence.replace(word, replacement.repeat(word.length));
   }
 }
 
