ae4cad60fdc3344b5c30a359e40dbf3609b62a5e
Jonathan Martins <dejimartins99@gmail.com>
2022-07-27 20:58:51 +0100

    trial

--- trial.js
+++ trial.js
@@ -1,11 +1,17 @@
-console.log("Hello world");
-
 let sentence = "Hel World Bro Beast";
+
+//List of the words to replace
 let toScramble = "Hel Beast";
 toScramble = toScramble.split(" ");
+
+//what to replace the word with
 let replacement = "*";
+
+//More information
 let numberOfWordsScrambled = toScramble.length;
 let numberOfscrambledCharacters = 0;
+
+//Replace process
 for (word of toScramble) {
   if (sentence.includes(word)) {
     numberOfscrambledCharacters += word.length;
@@ -13,6 +19,7 @@ for (word of toScramble) {
   }
 }
 
+//Output
 console.log(sentence);
 console.log(numberOfWordsScrambled);
 console.log(numberOfscrambledCharacters);
